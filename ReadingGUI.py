3d936f449a6f2ce356233ab964317fdf9d938aa0
Sandsnes <tarjei_sandsnes@outlook.com>
2018-10-31 12:07:38 +0100

    Restarted the GUI

--- ReadingGUI.py
+++ ReadingGUI.py
@@ -6,30 +6,34 @@ Created on Tue Oct 30 19:27:49 2018
 """
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget,
+                             QAction, QTableWidget,QTableWidgetItem,QVBoxLayout, QLabel, QLineEdit, QPushButton)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
 class ReadingApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = 'You and your Reading'
-        self.left = 300
-        self.top = 500
-        self.width = 1000
-        self.height = 500
+        
         self.initUI()
         
     def initUI(self):
-        self.setWindowTitle(self.title)
+        self.setWindowTitle("My Reading App")
+        self.left = 50
+        self.top = 100
+        self.width = 1000
+        self.height = self.width
         self.setGeometry(self.left, self.top, self.width, self.height)
- 
-        #self.createTable()
- 
-        # Add box layout, add table to box layout and add box layout to widget
-        self.layout = QVBoxLayout()
-        #self.layout.addWidget(self.tableWidget) 
-        self.setLayout(self.layout) 
+        btn = QPushButton('Update Progress', self)
+        btn.setToolTip('Update your reading progress')
+        btn.resize(btn.sizeHint())
+        btn.move(50,self.height-250)
+        btn_add_books = QPushButton('Add a New Book', self)
+        btn_add_books.setToolTip ('Add a new book to your shelf')
+        btn_add_books.resize(btn.sizeHint())
+        btn_add_books.move(50, self.height-100) #Add the bottom of the app
+        
+        
  
         # Show widget
         self.show()
