2360b6457d9e42785771114348ea5e397a98a5a6
Sandsnes <tarjei_sandsnes@outlook.com>
2018-11-08 20:48:53 +0100

    Fixed buttons to adapt to changing window size

--- ReadingGUI.py
+++ ReadingGUI.py
@@ -7,7 +7,7 @@ Created on Tue Oct 30 19:27:49 2018
 
 import sys
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget,
-                             QAction, QTableWidget,QTableWidgetItem,QVBoxLayout,
+                             QAction, QTableWidget,QTableWidgetItem,QVBoxLayout, QHBoxLayout,
                              QLabel, QLineEdit, QPushButton, qApp)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
@@ -19,21 +19,27 @@ class ReadingApp(QMainWindow):
         self.initUI()
         
     def initUI(self):
-        self.setWindowTitle("Reading & Me")
         
-        self.left = 50
-        self.top = 100
-        self.width = 1000
-        self.height = self.width
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        btn = QPushButton('Update Progress', self)
-        btn.setToolTip('Update your reading progress')
-        btn.resize(btn.sizeHint())
-        btn.move(50,self.height-250)
+        btn_progress = QPushButton('Update Progress', self)
+        btn_progress.setToolTip('Update your reading progress')
+        btn_progress.resize(btn_progress.sizeHint())
         btn_add_books = QPushButton('Add a New Book', self)
         btn_add_books.setToolTip ('Add a new book to your shelf')
-        btn_add_books.resize(btn.sizeHint())
-        btn_add_books.move(50, self.height-100) #Add the bottom of the app
+        btn_add_books.resize(btn_add_books.sizeHint())
+        
+        
+        hbox = QHBoxLayout()
+        hbox.addWidget(btn_progress)
+        hbox.addWidget(btn_add_books)
+        
+        
+        vbox = QVBoxLayout() 
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        
+        wid = QWidget(self)
+        self.setCentralWidget(wid)
+        wid.setLayout(vbox)
         
         exitAct = QAction(QIcon('exit24.png'), '&Exit', self)
         exitAct.setShortcut('Ctrl+Q') #Short cut for exiting app
@@ -53,6 +59,13 @@ class ReadingApp(QMainWindow):
         
         # Show widget
         self.setWindowIcon(QIcon('readinglogo.jpg'))
+        
+        self.left = 50
+        self.top = 100
+        self.width = 1000
+        self.height = self.width
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowTitle("Reading & Me")
         
         self.show()
     
