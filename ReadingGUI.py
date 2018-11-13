6e8b50d0fd9686081100452e5561a24b018b26e3
Sandsnes <tarjei_sandsnes@outlook.com>
2018-11-13 13:37:30 +0100

    Redesigned plus multiple adjustments

--- ReadingGUI.py
+++ ReadingGUI.py
@@ -8,7 +8,7 @@ Created on Tue Oct 30 19:27:49 2018
 import sys
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget,
                              QAction, QTableWidget,QTableWidgetItem,QVBoxLayout, QHBoxLayout,
-                             QLabel, QLineEdit, QPushButton, 
+                             QLabel, QLineEdit, QPushButton, QTabWidget,
                              qApp, QInputDialog, QFileDialog, QTextEdit, QComboBox)
                              
 from PyQt5.QtGui import QIcon
@@ -23,34 +23,64 @@ class ReadingApp(QMainWindow):
         
     def initUI(self):
         
-        self.btn_progress = QPushButton('Update Progress', self) 
+        self.btn_progress = QPushButton('Update Progress', self) #Add Progress button
         self.btn_progress.setToolTip('Update your reading progress')
-        self.btn_add_books = QPushButton('Add a New Book', self)
+        
+        self.btn_add_books = QPushButton('Add a New Book', self) ###Add a new Book button
         self.btn_add_books.setToolTip ('Add a new book to your shelf')
-        self.btn_add_books.clicked.connect (self.showDialog) #Opens up a dialog where you can "add" books
+        #self.btn_add_books.clicked.connect (self.showDialog) #Opens up a dialog where you can "add" books
+        self.btn_add_books.clicked.connect(self.addBooksFunc)
         
-        self.added_book = QLabel('', self)
+        self.added_book = QLabel('', self) #Label indicating added book 
         
-        self.addNotes = QTextEdit() #
+        self.add_booksLbl = QLabel("Enter the details of a new book below:")
+        #self.addNotes = QTextEdit() #
         
-        self.combo = QComboBox(self) ###May be useful for selecting book "currently reading"
+        self.addTitle = QLineEdit()
+        self.addTitleLbl = QLabel("Title",self)
+        titleHbox = QHBoxLayout()
+        titleHbox.addWidget(self.addTitle)
+        titleHbox.addWidget(self.addTitleLbl)
+        
+        self.addAuthor = QLineEdit()
+        self.addAuthorLbl = QLabel("Author", self)
+        AuthorHbox = QHBoxLayout()
+        AuthorHbox.addWidget(self.addAuthor)
+        AuthorHbox.addWidget(self.addAuthorLbl)
+        
+        self.addPages = QLineEdit()
+        self.addPages.resize(50,20)
+        self.addPagesLbl = QLabel("Pages", self)
+        PagesHbox = QHBoxLayout()
+        PagesHbox.addWidget(self.addPages)
+        PagesHbox.addWidget(self.addPagesLbl)
+        
+        #Choose book to update progress on
+        self.combo = QComboBox(self) 
         self.combo.addItem("Book1")
         self.combo.activated[str].connect(self.onActivated) 
         
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.btn_progress)
-        hbox.addWidget(self.btn_add_books)
+        ProgresshBox = QHBoxLayout()
+        ProgresshBox.addWidget(self.combo)
+        ProgresshBox.addWidget(self.btn_progress)
         
+        #####Add first tab, add books or add progress + books currently reading
         vbox = QVBoxLayout() 
-        vbox.addWidget(self.addNotes)
+        #vbox.addWidget(self.addNotes)
+        vbox.addWidget(self.add_booksLbl)
+        vbox.addLayout(titleHbox)
+        vbox.addLayout(AuthorHbox)
+        vbox.addLayout(PagesHbox)
+        vbox.addWidget(self.btn_add_books)
+        vbox.addWidget(self.added_book) 
         vbox.addStretch(1)
-        vbox.addWidget(self.combo)
-        vbox.addLayout(hbox)
-        vbox.addWidget(self.added_book) #Is aded below the horizontal book
+        vbox.addLayout(ProgresshBox)
         
-        wid = QWidget(self)
-        self.setCentralWidget(wid)
-        wid.setLayout(vbox)
+        
+        tab1 = QWidget(self) #First Tab
+        self.setCentralWidget(tab1)
+        tab1.setLayout(vbox)
+        
         
         exitAct = QAction(QIcon('exit24.png'), '&Exit', self)
         exitAct.setShortcut('Ctrl+Q') #Short cut for exiting app
@@ -112,6 +142,21 @@ class ReadingApp(QMainWindow):
     def onActivated(self, text):
     
         self.btn_progress.setText("Update the progress of: " + text)
+        
+    def addBooksFunc(self):
+        '''Adds book to combo widget and changes label at the bottom to indicate added book'''
+        '''Also adds these details to data file ###To be added####'''
+        title = self.addTitle.text()
+        author = self.addAuthor.text()
+        if title != "":
+            if author != "":
+                self.added_book.setText("You have just added " + str(title) + " by " + str(author) +
+                                        " to your reading list")
+                self.combo.addItem(str(title))
+            else:
+                self.added_book.setText("Please add the author")
+        else:
+            self.added_book.setText("Please add a title")
         
     
 
